85e81c94d9554c2972e390768585157ad955ac96
jzhowie <jzhou20@stuy.edu>
2020-11-21 13:37:58 -0500

    Initialized add and subtract method (overrides RealNumber)

--- RationalNumber.java
+++ RationalNumber.java
@@ -124,12 +124,14 @@ public RationalNumber divide(RationalNumber other){
  * Return a new RationalNumber that is the sum of this and the other
  */
 public RationalNumber add(RationalNumber other){
-	return null;
+	RationalNumber sum = new RationalNumber (getNumerator() * other.getDenominator() + other.getNumerator() * getDenominator(), getDenominator() * other.getDenominator());
+	return sum;
 }
 /**
  * Return a new RationalNumber that this minus the other
  */
 public RationalNumber subtract(RationalNumber other){
-	return null;
+	RationalNumber diff = new RationalNumber (getNumerator() * other.getDenominator() - other.getNumerator() * getDenominator(), getDenominator() * other.getDenominator());
+	return diff;
 }
 }
